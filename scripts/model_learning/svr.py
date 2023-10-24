23b01b5c2c606c5a681aa889d1c4980674434256
yashka2210 <yashka2210@ml-srv.myguest.virtualbox.org>
2023-10-25 00:59:47 +0500

    add model train

---
+++ scripts/model_learning/svr.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.svm import SVR
+import pickle
+import os
+# SVM
+
+from sklearn.model_selection import StratifiedKFold
+
+from sklearn.metrics import mean_squared_error as mse, r2_score
+
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import cross_validate
+
+from sklearn.svm import SVC
+
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.utils.fixes import loguniform
+
+epsilon=20 
+
+C=100 
+
+kernel = 'poly' 
+coef0=4.5 
+degree=3 
+gamma=0.4 
+
+if gamma == 0:
+  gamma='auto'
+
+SVM_reg=SVR(kernel=kernel, epsilon=epsilon, C=C, gamma=gamma,
+            degree=degree, coef0 = coef0)
+
+X_Train = pd.read_csv('data/stage_2/X_Train.csv')
+
+y_Train = pd.read_csv('data/stage_2/y_Train.csv')
+
+SVM_reg.fit(X_Train, y_Train)
+
+with open("models", "wb") as fd:
+  pickle.dump(SVM_reg, fd)
