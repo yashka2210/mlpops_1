549f16ddea151e0a25c48a66783018688b977795
yashka2210 <yashka2210@ml-srv.myguest.virtualbox.org>
2023-10-24 23:36:05 +0500

    data processing pipeline

--- scripts/data_preprocessing/get_features_n_split.py
+++ scripts/data_preprocessing/get_features_n_split.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
+import os
 
-df = pd.read_csv('/wines_SPA.csv', delimiter = ',')
+df = pd.read_csv('data/raw/wines_SPA.csv', delimiter = ',')
 df['year'] = df['year'].replace('N.V.', np.NaN)  #заменяем в датафрейме реплейсом
 df['body'] = df['body'].replace('<NA>', np.NaN)
 df['acidity'] = df['acidity'].replace('<NA>', np.NaN)
@@ -12,4 +14,12 @@ df_year_num = df.astype({"year": "Int64"})
 df_year_num['extract'] = [2022-x for x in df_year_num["year"]] #заполняем новосозданый столбец, пробегая по значениям года
 df_year_num.drop(columns = ['year'],inplace=True)  #Удаляем год
 df = df_year_num
-df.drop(columns=['country','year','acidity'],inplace=True)
+df.drop(columns=['country','acidity'],inplace=True)
+X_Train, X_test, y_Train, y_test = train_test_split(df.drop(columns=['price']), df.price.values.ravel(), test_size=0.3, random_state=42)
+os.makedirs(os.path.join("data", "stage_1"),exist_ok=True)
+X_Train.to_csv('data/stage_1/X_Train.csv')
+X_test.to_csv('data/stage_1/X_test.csv')
+y_Train = pd.DataFrame(y_Train)
+y_test = pd.DataFrame(y_test)
+y_Train.to_csv('data/stage_1/y_Train.csv')
+y_test.to_csv('data/stage_1/y_test.csv')
---
+++ scripts/data_preprocessing/preprocessing.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import os
+X_Train = pd.read_csv('data/stage_1/X_Train.csv')
+X_test = pd.read_csv('data/stage_1/X_test.csv')
+y_Train = pd.read_csv('data/stage_1/y_Train.csv')
+y_test = pd.read_csv('data/stage_1/y_test.csv')
+
+
+cat_columns = []
+num_columns = []
+
+for column_name in X_Train.columns:
+    if (X_Train[column_name].dtypes == object):
+        cat_columns +=[column_name]
+    else:
+        num_columns +=[column_name]
+
+print('categorical columns:\t ',cat_columns, '\n len = ',len(cat_columns))
+
+print('numerical columns:\t ',  num_columns, '\n len = ',len(num_columns))
+
+from sklearn.impute import SimpleImputer # Объект для замены пропущенных значений
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler # Импортируем нормализацию и One-Hot Encoding от scki-kit-learn
+from sklearn.pipeline import Pipeline # Pipeline.Не добавить, не убавить
+from sklearn.compose import ColumnTransformer # т.н. преобразователь колонок
+
+numerical_pipe = Pipeline([
+    ('scaler', MinMaxScaler())   #нормализуем числовые минмакс скалером
+])
+
+categorical_pipe = Pipeline([
+    ('encoder', OneHotEncoder(drop='if_binary', handle_unknown='ignore', sparse_output=False)) #категориальные кодируем ванхотом
+])
+
+preprocessors = ColumnTransformer(transformers=[   #преобразуем в красоту
+    ('num', numerical_pipe, num_columns),
+    ('cat', categorical_pipe, cat_columns)
+])
+
+preprocessors.fit(X_Train)
+
+X_train = preprocessors.transform(X_Train) # преобразуем  тренировочные данные
+
+X_test = preprocessors.transform(X_test) # преобразуем  тестовые данные
+
+X_train = pd.DataFrame(X_train)
+X_test = pd.DataFrame(X_test)
+
+os.makedirs(os.path.join("data", "stage_2"),exist_ok=True)
+
+X_Train.to_csv('data/stage_2/X_Train.csv')
+X_test.to_csv('data/stage_2/X_test.csv')
+y_Train.to_csv('data/stage_2/y_Train.csv')
+y_test.to_csv('data/stage_2/y_test.csv')
