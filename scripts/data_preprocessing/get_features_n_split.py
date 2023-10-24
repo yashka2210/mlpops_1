68a05a9de515d75196e1c682da16861a53c6e347
yashka2210 <yashka2210@ml-srv.myguest.virtualbox.org>
2023-10-25 00:30:05 +0500

    add params in script

--- scripts/data_preprocessing/get_features_n_split.py
+++ scripts/data_preprocessing/get_features_n_split.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import os
+import yaml
 
 df = pd.read_csv('data/raw/wines_SPA.csv', delimiter = ',')
 df['year'] = df['year'].replace('N.V.', np.NaN)  #заменяем в датафрейме реплейсом
@@ -15,7 +16,11 @@ df_year_num['extract'] = [2022-x for x in df_year_num["year"]] #заполняе
 df_year_num.drop(columns = ['year'],inplace=True)  #Удаляем год
 df = df_year_num
 df.drop(columns=['country','acidity'],inplace=True)
-X_Train, X_test, y_Train, y_test = train_test_split(df.drop(columns=['price']), df.price.values.ravel(), test_size=0.3, random_state=42)
+
+params = yaml.safe_load(open("params.yaml"))["split"]
+p_split_ratio = params["split_ratio"]
+
+X_Train, X_test, y_Train, y_test = train_test_split(df.drop(columns=['price']), df.price.values.ravel(), test_size=p_split_ratio, random_state=42)
 os.makedirs(os.path.join("data", "stage_1"),exist_ok=True)
 X_Train.to_csv('data/stage_1/X_Train.csv')
 X_test.to_csv('data/stage_1/X_test.csv')
